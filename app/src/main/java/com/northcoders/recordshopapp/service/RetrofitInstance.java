79be51130dffb69f4f7b1ef3a49c40936617b62c
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-06-20 12:04:04 +0100

    add RetrofitInstance.java add getService() and BASE_URL

--- app/src/main/java/com/northcoders/recordshopapp/service/RetrofitInstance.java
+++ app/src/main/java/com/northcoders/recordshopapp/service/RetrofitInstance.java
@@ -1,7 +1,19 @@
 package com.northcoders.recordshopapp.service;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
     private static Retrofit retrofit = null;
+
+    private static final String BASE_URL = "http://10.0.2.2:8080/api/v1/";
+
+    public static AlbumApiService getService(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit.create(AlbumApiService.class);
+    }
 }
