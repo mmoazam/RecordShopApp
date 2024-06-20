d8ceb09390f42ec488fe61b262d66d3322bb6f89
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-06-20 12:20:13 +0100

    impl: getMutableLiveData()

---
+++ app/src/main/java/com/northcoders/recordshopapp/model/AlbumRepository.java
@@ -0,0 +1,41 @@
+package com.northcoders.recordshopapp.model;
+
+import android.app.Application;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.northcoders.recordshopapp.service.AlbumApiService;
+import com.northcoders.recordshopapp.service.RetrofitInstance;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AlbumRepository {
+    private MutableLiveData<List<Album>> mutableLiveData;
+    private Application application;
+
+    public AlbumRepository(Application application) {
+        this.application = application;
+    }
+
+    public MutableLiveData<List<Album>> getMutableLiveData() {
+        AlbumApiService albumApiService = RetrofitInstance.getService();
+        Call<List<Album>> call= albumApiService.getAlbums();
+        call.enqueue(new Callback<List<Album>>() {
+            @Override
+            public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
+                List<Album> albums = response.body();
+                mutableLiveData.setValue(albums);
+            }
+
+            @Override
+            public void onFailure(Call<List<Album>> call, Throwable t) {
+
+            }
+        });
+        return mutableLiveData;
+    }
+}
