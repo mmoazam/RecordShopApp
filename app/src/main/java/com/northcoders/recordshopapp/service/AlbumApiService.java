e6cf4eb0860cc58caa4a9c920b8cf7873e4b84b3
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-06-21 11:02:38 +0100

    add addAlbum() method

--- app/src/main/java/com/northcoders/recordshopapp/service/AlbumApiService.java
+++ app/src/main/java/com/northcoders/recordshopapp/service/AlbumApiService.java
@@ -5,9 +5,14 @@ import com.northcoders.recordshopapp.model.Album;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface AlbumApiService {
     @GET("albums")
     Call<List<Album>> getAllAlbums();
+
+    @POST("albums")
+    Call<Album> addAlbum(@Body Album album);
 }
