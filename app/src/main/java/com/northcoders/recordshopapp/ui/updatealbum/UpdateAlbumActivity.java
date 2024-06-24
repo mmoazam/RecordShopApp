d358e8ab16727b5bc58298b6159a0a76a452410f
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-06-24 14:54:04 +0100

    impl: UpdateAlbumClickHandlers

--- app/src/main/java/com/northcoders/recordshopapp/ui/updatealbum/UpdateAlbumActivity.java
+++ app/src/main/java/com/northcoders/recordshopapp/ui/updatealbum/UpdateAlbumActivity.java
@@ -7,10 +7,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.northcoders.recordshopapp.R;
+import com.northcoders.recordshopapp.databinding.ActivityUpdateAlbumBinding;
+import com.northcoders.recordshopapp.model.Album;
+import com.northcoders.recordshopapp.ui.mainactivity.MainActivityViewModel;
 
 public class UpdateAlbumActivity extends AppCompatActivity {
+
+    private ActivityUpdateAlbumBinding binding;
+    private UpdateAlbumClickHandlers clickHandlers;
+    private Album album;
+    private static final String ALBUM_KEY = "album";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +32,16 @@ public class UpdateAlbumActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        album = getIntent().getParcelableExtra(ALBUM_KEY, Album.class);
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_update_album
+        );
+
+        MainActivityViewModel viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        clickHandlers = new UpdateAlbumClickHandlers(viewModel, album, this);
+        binding.setAlbum(album);
+        binding.setHandlers(clickHandlers);
     }
 }
