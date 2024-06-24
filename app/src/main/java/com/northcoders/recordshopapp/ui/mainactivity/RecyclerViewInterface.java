014c02186d73d4c8e151d18e4dc994636dc9fca1
mmoazam <14061746+mmoazam@users.noreply.github.com>
2024-06-24 12:56:44 +0100

    add onItemClicked()

--- app/src/main/java/com/northcoders/recordshopapp/ui/mainactivity/RecyclerViewInterface.java
+++ app/src/main/java/com/northcoders/recordshopapp/ui/mainactivity/RecyclerViewInterface.java
@@ -1,4 +1,7 @@
 package com.northcoders.recordshopapp.ui.mainactivity;
 
+import com.northcoders.recordshopapp.model.Album;
+
 public interface RecyclerViewInterface {
+    public void onItemClicked(int position);
 }
